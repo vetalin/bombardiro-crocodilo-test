ec2e168b8b5f9e5518c50247511b5eb8074642fc
vetal <vetal-frontend@yandex.ru>
2025-05-11 17:02:26 +0300

    Добавлены новые зависимости в package.json и package-lock.json, включая @tailwindcss/vite, class-variance-authority, clsx, lucide-react, shadcn-ui, tailwind-merge, а также обновлены devDependencies. В tsconfig.json и tsconfig.node.json добавлены пути для упрощения импорта. Обновлен файл конфигурации Vite для поддержки Tailwind CSS и добавлен алиас для src.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 function handleModuleDirectivesPlugin() {
   return {
@@ -30,8 +32,13 @@ export default defineConfig({
     legacy({
       targets: ['defaults', 'not IE 11'],
     }),
+    tailwindcss(),
   ],
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'build',
   },
